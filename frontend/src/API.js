f34fec58822be6e05000b5e71a1fbc6fe5f124b3
sajid-techis <sajid@techis.io>
2022-01-20 01:48:40 +0530

    added api

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 var baseURL;
-// if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
-//     baseURL = process.env.REACT_APP_API_BASE_URL;
-// } else {
-//     baseURL = 'http://127.0.0.1:8000';
-// }
-baseURL = 'https://sajid-symantic-backend.herokuapp.com/';
+if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
+    baseURL = process.env.REACT_APP_API_BASE_URL;
+} else {
+    baseURL = 'http://127.0.0.1:8000';
+}
 
 const api = axios.create({
     baseURL: baseURL,
