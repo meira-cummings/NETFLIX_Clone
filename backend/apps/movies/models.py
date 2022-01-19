5ab5e71042d66488f78f90321dcc0bd9d0010b32
M.A.C <93954303+meira-cummings@users.noreply.github.com>
2022-01-19 12:56:44 -0800

    Update models.py

--- backend/apps/movies/models.py
+++ backend/apps/movies/models.py
@@ -23,7 +23,7 @@ class Movie(models.Model):
         'image mobile', blank=True, null=True
     )
     release_type = models.CharField(
-        'release_type', blank=False, null=True, max_length=50, choices= MY_CHOICES
+        'release_type', blank=True, null=True, max_length=50, choices= MY_CHOICES
     )
     category_id = models.ForeignKey(
         Category, on_delete=models.CASCADE
